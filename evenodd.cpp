34905e99b7622dc2b2b79b65df6e79386ebfeffb
SRRrain750 <160513264+SRRrain750@users.noreply.github.com>
2024-02-19 22:24:18 +0530

    Update evenodd.cpp
    first contribution abhishek parochi
    date 19-2-24
    time-22:25

--- evenodd.cpp
+++ evenodd.cpp
@@ -1,9 +1,14 @@
+/*  MAKE CHANGES
+BY ABHISHEKN PAROCHI
+DATE/TIME: 19-02-2024*/
+
+
 #include<iostream>
 using namespace std;
 int main(){
           int  num1[6]={7,9,9,9,4,7};
-          int  num2 [6]={null};
-          int  num3[6]={null};
+          int  num2 [6]={NULL};
+          int  num3[6]={NULL};
           for(int i=0; i<6; i++){
                if( num1[i]%2==0){
                  num2[i]=num1[i];
@@ -15,11 +20,15 @@ int main(){
 
           }
           for(int j=0; j<6; j++ ){
-            cout<<num2[j]<<" ";
+            if(num2[j]!=NULL){
+                cout<<num2[j]<<" ";
+            }
           }
           cout<<endl;
           for(int k=0; k<6; k++){
-            cout<<num3[k]<<" ";
+            if(num3[k]!=NULL){
+                cout<<num3[k]<<" ";
+            }
           }
 
 

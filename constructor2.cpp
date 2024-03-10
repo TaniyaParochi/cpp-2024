586b56f596675c66291102edab60dcc3f37a213d
SRRrain750 <160513264+SRRrain750@users.noreply.github.com>
2024-03-10 21:47:17 +0530

    Add files via upload

---
+++ constructor2.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+class maoblie{
+
+public:
+    string name;
+    int price;
+
+ moblie(){
+
+ cout<<"enter the moblie name:";
+ cin>>name;
+ cout<<"enter the moblie price:";
+ cin>>price;
+
+ }
+
+disp_moblie(){
+
+cout<<"\n your moblie name is:"<<name;
+cout<<"\n your moblie price is:"<<price;
+}
+};
+
+
+ int main(){
+
+
+moblie mob1;
+mob1.disp_moblie();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

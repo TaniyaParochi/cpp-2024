85fa3d2374496125bd1a38111b1cbead10868af1
TaniyaParochi <163005378+TaniyaParochi@users.noreply.github.com>
2024-03-11 22:37:27 +0530

    corrected code

--- calculetor.cpp
+++ calculetor.cpp
@@ -10,10 +10,9 @@ int main(){
 add(); // call
  sub(); //call
  mul();// call 
-
+ return 0;
  
 }
-
 
 
 void add(void){
@@ -26,9 +25,9 @@ void add(void){
     cin>>num2;
     sum=num1+num2;
     cout<<"your sum is"<<sum;
-
  }
-
+ 
+ 
  void sub(void){
     int sub;
     int num1;
@@ -37,14 +36,11 @@ void add(void){
     cin>>num1;
     cout<<"enter the secend num";
     cin>>num2;
-    sum=num1+num2;
+    sub=num1+num2;
     cout<<"your sub is"<<sub;
-
-
  }
-
-
-
+ 
+ 
  void mul(void){
     int mul;
     int num1;
@@ -56,5 +52,4 @@ void add(void){
     mul=num1*num2;
     cout<<"your mul is"<<mul;
  }
-return 0;
- }
+

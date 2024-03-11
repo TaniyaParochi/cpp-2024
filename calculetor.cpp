b5cc726e076455f4bb37467a39c566c0b2b60af4
SRRrain750 <160513264+SRRrain750@users.noreply.github.com>
2024-03-11 21:58:38 +0530

    Add files via upload

---
+++ calculetor.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+   int a,b;
+   int sum;
+   int sub;
+   int mul;
+
+while(){
+
+ cout<<"add the numbers";
+    sum=a+b;
+cin>>sum;
+
+
+cout<<"sub the numbers";
+    sub=a-b;
+cin>>sub;
+
+
+
+cout<<"mul the numbers";
+    mul=a*b;
+cin>>mul;
+
+
+
+
+}
+return 0;
+}
